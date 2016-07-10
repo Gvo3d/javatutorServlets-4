e621b53d52434c6ad6510f26ffa3aa60e7032bc7
Gvo3d <denis.yakimov89@gmail.com>
2016-07-10 15:05:40 +0300

    Initial commit

---
+++ src/main/java/SessionIntervalServlet.java
@@ -0,0 +1,31 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Gvozd on 10.07.2016.
+ */
+public class SessionIntervalServlet extends HttpServlet{
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        HttpSession session = req.getSession();
+        session.setMaxInactiveInterval(5);
+
+
+        out.println("Session interval is "+ session.getMaxInactiveInterval());
+
+        Integer ival = (Integer) session.getAttribute("sesspeek.cntr");
+        if (ival == null)
+            ival = new Integer(1);
+        else
+            ival = new Integer(ival.intValue() + 1);
+        session.setAttribute("sesspeek.cntr", ival);
+        out.println("You have hit this page " + ival + " times.<p>");
+        out.close();
+    }
+}
